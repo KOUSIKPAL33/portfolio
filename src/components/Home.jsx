64adbc2c0460b07b239d273d4b4aaa5698316ef3
KOUSIK PAL <144161665+KOUSIKPAL33@users.noreply.github.com>
2025-03-16 10:42:25 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -37,7 +37,7 @@ const Home = () => {
 
           <div className="projects-btn">
             <Link
-              to="Projects"
+              to="projects"
               smooth
               duration={500}
               className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
